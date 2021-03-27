c79a6308542a479cbddcfe8acbfb5b891f378828
yiregoo <56330506+yiregoo@users.noreply.github.com>
2021-03-27 16:21:37 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,75 @@
+//function
+
+function hey(numbers){
+
+var cost = numbers * 5;
+console.log("buy " + numbers);
+
+}
+
+hey(13);
+
+
+
+//
+
+
+function hey(money){
+
+var numberOfThings = money / 2;
+console.log("buy " + numberOfThings);
+
+}
+
+hey(13);
+
+//
+
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    
+    
+    var days = (90-age)*365;
+    var weeks = (90-age)*52;
+    var months = (90-age)*12;
+    
+    console.log("You have " + days +" days, " + weeks+" weeks, and " + months + " months left.");
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
+lifeInWeeks(51);
+
+
+//outputs & Return; return as output
+//Modulo
+
+ 
+
+function calculator(){
+     var weight;
+   var height;
+   var bmi = bmiCalculator(65, 1.8); 
+    
+    
+}
+
+function bmiCalculator(weight, height){
+    
+  var bmi = weight/(height*height);
+  return bmi;
+    
+    
+}
+
+console.log("Your bmi is " + bmi);
+
+
+
+//
+
